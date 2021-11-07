6e5ec766100a0027a1e9c5a385d7ce866ac8ab7f
Gnaneswar Rao Mantri <gnanimail@gmail.com>
2021-11-07 20:43:44 +0530

    Update preprocess.py

--- preprocess_data/preprocess.py
+++ preprocess_data/preprocess.py
@@ -1,14 +1,13 @@
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def _preprocess_data():
-     subset_ = pd.read_csv('../data/subsetted.csv', encoding='cp1252')
+     subset_ = pd.read_csv('Emissions_data.csv', encoding='cp1252')
      X = subset_.drop('Nox_EI_C/O', axis=1)
      y = subset_.iloc[:, -1]
 
-     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
+     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
      np.save('x_train.npy', X_train)
      np.save('x_test.npy', X_test)
      np.save('y_train.npy', y_train)
